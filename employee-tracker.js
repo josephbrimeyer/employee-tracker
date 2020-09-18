2bb85c50cab754bbbb7e2d5e049d3d8a57a68ade
Joe Brimeyer <jbrimeyer@gmail.com>
2020-09-18 11:38:45 -0700

    added second connection.

--- employee-tracker.js
+++ employee-tracker.js
@@ -14,6 +14,14 @@ let connection = mysql.createConnection({
   database: "employee_trackerDB",
 });
 
+let connectionTwo = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "employee_trackerDB",
+});
+
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
